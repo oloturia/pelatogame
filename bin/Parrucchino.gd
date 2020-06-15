abde7e3df750177e9872fbe4a04af0e06f19704a
oloturia <oloturia@gmail.com>
2020-06-16 01:55:18 +0200

    added particles, tutorial

--- bin/Parrucchino.gd
+++ bin/Parrucchino.gd
@@ -1,9 +1,17 @@
 extends KinematicBody2D
 
+var gotcha = false
 
 func _ready():
 	pass 
 
 
 func _on_Area2D_area_shape_entered(_area_id, _area, _area_shape, _self_shape):
-	queue_free()
+	$Stars.emitting = true
+	$AnimatedSprite.visible = false
+	gotcha = true
+
+
+func _process(_delta):
+	if gotcha and not $Stars.emitting:
+		queue_free()
